5f92f372cdb4f7c34b1ea3a483866542120a8efd
Luis David <luisdavidmoreta12@gmail.com>
2025-09-26 23:37:45 -0400

    Add files via upload

---
+++ ETL_OpinionesClientes/ETL_OpinionesClientes/Fuente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL_OpinionesClientes
+{
+    
+        public class Fuente
+        {
+            public string IdFuente { get; set; }
+            public string TipoFuente { get; set; }
+            public DateTime FechaCarga { get; set; }
+        }
+    
+}
---
+++ ETL_OpinionesClientes/ETL_OpinionesClientes/ResenaWeb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL_OpinionesClientes
+{
+    public class ResenaWeb
+    {
+        public string IdReview { get; set; }
+        public string IdCliente { get; set; }
+        public string IdProducto { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Comentario { get; set; }
+        public int Rating { get; set; }
+    }
+}
